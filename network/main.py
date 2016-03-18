8fbbbb5a1e4e7abb65810ca8fe0b9ca7c2e42568
keerthivassan86 <keerthivassan86@gmail.com>
2016-03-18 13:20:43 +0530

    Network interface changes

--- network/main.py
+++ network/main.py
@@ -4,6 +4,11 @@ import pdb
 from ucsmsdk import *
 from ucsmsdk import ucshandle
 from config import IP,USERNAME,PASSWORD
+
+"""
+Identifying Network interface changes inside UCSM
+"""
+
 def main():
 
     handle=ucshandle.UcsHandle(IP,username=USERNAME,password=PASSWORD)
