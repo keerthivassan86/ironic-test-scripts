8a016930475d851255f1666d62231da90e1818b4
openstackdev86 <openstackdev86@gmail.com>
2016-04-02 13:05:12 +0530

    creating vlan and assignined to the vnics.

--- network/Network.py
+++ network/Network.py
@@ -2,6 +2,8 @@ import os
 import time
 import pdb
 from ucsmsdk import *
+from ucsmsdk.mometa.fabric.FabricVlan import FabricVlan
+from ucsmsdk.mometa.vnic.VnicEtherIf import VnicEtherIf
 
 
 class Network(object):
@@ -33,28 +35,56 @@ class Network(object):
         if sp_temp:
             for s in sp_temp:
                 print("service profile template <%s> found"%(s['name']))
-                blade_sp=self.discover_blade_sp()
+
+                """
+                List the service profile that is only associated with blades.
+                """
+                blade_sp=self.discover_blade_sp()                 
 
                 for blade in blade_sp:
                     pdb.set_trace()
                     blade_assinment=blade['blade']['assigned_to']
                     print("Vnic Ethernet information")
+                    """
+                    List all vnics under service profile.
+                    """
                     vnic_ether=self.handle.query_children(in_dn=blade_assinment,class_id="VnicEther")
                     for vnic in vnic_ether:
                         vnicc={'dn':vnic.dn,'vnic_name':vnic.name}
                         print "Fetching Vlan information for the VNIC %s"%(blade_assinment+"/ether-"+vnic.name)
                         mydn=blade_assinment + "/ether-" +vnic.name
+                        """
+                        List all vlans under particular vnic.
+                        """
                         vlan_ether=self.handle.query_children(in_dn=mydn,class_id="vnicEtherIf")
                         for vlan in vlan_ether:
                             vlann={'vlan':{'vnic_name':blade_assinment+"/ether-"+vnic.name,'vlan_name':vlan.name,'vlan_net':vlan.vnet,'vnet_dn':vlan.oper_vnet_dn}}
-                            print vlann
+                            print("Available vlans under vnic %s"%(blade_assinment+"/ether-"+vnic.name))
+                            print("\n")
+
+                            print("** vlan_name  --- %s vlan_id %s"%(vlan.name,vlan.vnet))
+
+                            if vlan.name == "default":
+                                print("Vlan creation is in process")
+                                vlan_name="myvlan"
+                                """
+                                creating new vlan and assign to the vnic.
+                                """
+                                vlan_creation=vlan_create(self.handle,vlan_name,"4050",parent_dn="fabric/lan")
+                                if vlan_creation:
+                                    print("vlan %s created sucessfull"%(vlan_name))
+                                       
+                                    #vlan_delete(self.handle,name="myvlan",parent_dn="fabric/lan") # for deleting the vlan
+
+                                    modify_vlan_if(self.handle,dn=blade_assinment+"/ether-"+vnic.name,child_action=vlan.child_action,config_qualifier=vlan.config_qualifier,default_net="no",flt_aggr=vlan.flt_aggr,name=vlan_name,oper_state=vlan.oper_state,oper_vnet_dn=vlan.oper_vnet_dn,oper_vnet_name=vlan.oper_vnet_name,switch_id=vlan.switch_id,owner=vlan.owner,prop_acl=vlan.prop_acl,
+                                           pub_nw_id=vlan.pub_nw_id,rn=vlan.rn,sacl=vlan.sacl,
+                                           type=vlan.type,vnet="4050")
+
                         #vnics.append(vnicc)
-
-
         else:
             print("No service profile template found")
 
-        print vnics
+        
 
     def discover_blade_sp(self):
         blade_sp=[]
@@ -70,3 +100,46 @@ class Network(object):
         else:
             print "No blades associated with sp"
         return blade_sp
+
+def modify_vlan_if(handle,dn,child_action=None,config_qualifier='',default_net='no',flt_aggr=0,name= None,oper_state=None,oper_vnet_dn=None,
+                   oper_vnet_name=None,switch_id=None,owner=None,prop_acl=None,pub_nw_id=None,
+                   rn=None,sacl=None,type=None,vnet=None):
+    obj=handle.query_dn(dn)
+    if obj:
+        myvlan= VnicEtherIf(parent_mo_or_dn=obj,default_net=default_net,
+                            name=name)
+        handle.add_mo(myvlan,modify_present=True)
+        handle.commit()
+        return myvlan
+
+
+
+def vlan_create(handle, name, vlan_id, sharing="none",
+                mcast_policy_name="", compression_type="included",
+                default_net="no", pub_nw_name="", parent_dn="fabric/lan"):
+    obj = handle.query_dn(parent_dn)
+    if obj:
+        vlan = FabricVlan(parent_mo_or_dn=obj,
+                          sharing=sharing,
+                          name=name,
+                          id=vlan_id,
+                          mcast_policy_name=mcast_policy_name,
+                          policy_owner="local",
+                          default_net=default_net,
+                          pub_nw_name=pub_nw_name,
+                          compression_type=compression_type)
+
+        handle.add_mo(vlan, modify_present=True)
+        handle.commit()
+        return vlan
+
+def vlan_delete(handle, name=None,parent_dn="fabric/lan"):
+    dn = parent_dn + '/net-' + name
+    mo = handle.query_dn(dn)
+    if mo:
+        handle.remove_mo(mo)
+        handle.commit()
+    else:
+        raise ValueError("VLAN '%s' is not present" % dn)
+
+
